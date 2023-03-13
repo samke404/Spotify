63273450533aaa1a667390054e2aaff81081e1fc
samke404 <you@example.com>
2023-03-13 11:10:35 +0200

    Nav container

---
+++ spotifyClone/src/navigators/MainTabNavigator.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MainTabNavigator = () => {
+  return (
+    <View>
+      <Text>MainTabNavigator</Text>
+    </View>
+  )
+}
+
+export default MainTabNavigator
+
+const styles = StyleSheet.create({})
